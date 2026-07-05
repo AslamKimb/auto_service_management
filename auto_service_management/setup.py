3a4208a7581233d3b33b6b4ebdb7e2ed7466c6e4
Aslam <asl.kimb@gmail.com>
2026-07-05 18:42:12 +0300

    fix: add setup.py shim for bench CLI compatibility and gitignore bench-data

---
+++ auto_service_management/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="auto_service_management",
+    version="0.1.0",
+    description="Automobile Repair Management for Frappe/ERPNext",
+    author="Aslam Kimbugwe",
+    author_email="kimbugwe43@gmail.com",
+    packages=["auto_service_management"],
+    zip_safe=False,
+    include_package_data=True,
+)
